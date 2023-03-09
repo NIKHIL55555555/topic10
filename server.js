786ab79a1bb8a535cd4a3452d7418ed7d1145aa1
NIKHIL55555555 <nikhil4815.be21@chitkara.edu.in>
2023-03-09 20:07:58 +0530

    Files

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+const port =  3000;
+const base = '${__dirname}/public';
+
+app.use(express.static('public'));
+
+
+app.get('/', function (req, res)  {
+    res.sendFile('${base}/iot-application.html');
+});
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+});
